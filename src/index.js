d38ed98c925c6fd28a2ca77ad356c38ba306ca58
Alex <bangau.alexandru@gmail.com>
2020-07-08 13:10:09 +0200

    Fetch current location with openweather api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './assets/styling/style.scss';
+
+window.addEventListener('load', () => {
+  let long;
+  let lat;
+  let temperatureDescription = document.querySelector('.temperature-description');
+  let temperatureDegree = document.querySelector('.temperature-degree');
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+
+      const proxy = 'https://cors-anywhere.herokuapp.com/';
+      const api = `${proxy}https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&
+      exclude=minutely,hurly,daily&appid=830135a8b4487d127daad9365fad5297`;
+
+      fetch(api)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          const { temperature, summary } = data.currently;
+        });
+    });
+  }
+});
